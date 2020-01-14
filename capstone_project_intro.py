3b6a0f9e6dc865fff2fa7589f74f21b3821e9ffc
Mawaz <mirza.mawaz@gmail.com>
2020-01-14 14:31:38 +0500

    Update and rename capstone_project_intro.py to capstone_project.py

--- capstone_project_intro.py
+++
@@ -1,6 +0,0 @@
-<h1> IBM Data Science Professional Specialization
-<h2> Capstone Project: Applied Data Science
-<h3> This notebook will primarily be used for capstone project and its assignments.
-import pandas as pd
-import numpy as np
-
