d2c879d7bd2632a9266224341c9356186040d78e
Mawaz <mirza.mawaz@gmail.com>
2020-01-14 14:28:11 +0500

    Update capstone_project_intro.py

--- capstone_project_intro.py
+++ capstone_project_intro.py
@@ -1,6 +1,6 @@
-# IBM Data Science Professional Specialization
-## Capstone Project: Applied Data Science
-### This notebook will primarily be used for capstone project and its assignments.
+<h1> IBM Data Science Professional Specialization
+<h2> Capstone Project: Applied Data Science
+<h3> This notebook will primarily be used for capstone project and its assignments.
 import pandas as pd
 import numpy as np
 
